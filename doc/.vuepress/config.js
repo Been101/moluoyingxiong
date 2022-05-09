808068a085f7d4f43153fca2aee819790cb8f708
been <1043609130@qq.com>
2022-05-09 09:27:19 +0800

    feat: 添加 promise

--- doc/.vuepress/config.js
+++ doc/.vuepress/config.js
@@ -13,28 +13,37 @@ module.exports = {
     type: 'blog',
     mode: 'dark', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
     modePicker: false,
-    author: 'bean',
-    logo: '/head.png',
+    author: 'Bean',
+    logo: '/avatar.jpg',
     subSidebar: 'auto',
-    sidebar: [
+    nav: [
       {
-          title: '欢迎学习',
-          path: '/',
-          collapsable: false, // 不折叠
-          children: [
-            { title: "学前必读", path: "/" }
-          ]
-      },
-      {
-        title: "基础学习",
-        path: '/vue/ConditionalTypes',
-        collapsable: false, // 不折叠
-        children: [
-          { title: "条件类型", path: "/vue/ConditionalTypes" },
-          { title: "泛型", path: "/vue/Generics" }
-        ],
+        text: 'Node',
+        ariaLabel: 'Node Menu',
+        items: [
+          { text: 'Promise', link: '/node/Promise/' },
+        ]
       }
-    ]
+    ],
+    // sidebar: [
+    //   {
+    //       title: '欢迎学习',
+    //       path: '/',
+    //       collapsable: false, // 不折叠
+    //       children: [
+    //         { title: "学前必读", path: "/" }
+    //       ]
+    //   },
+    //   {
+    //     title: "基础学习",
+    //     path: '/vue/ConditionalTypes',
+    //     collapsable: false, // 不折叠
+    //     children: [
+    //       { title: "条件类型", path: "/vue/ConditionalTypes" },
+    //       { title: "泛型", path: "/vue/Generics" }
+    //     ],
+    //   }
+    // ]
   },
   dest: 'docs'
 }
